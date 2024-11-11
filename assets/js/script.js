4bee25459d2f3650f9f795268680f15d07f94963
Nizar Fazari <nizarfazari889@gmail.com>
2024-11-11 22:52:26 +0700

    Add Music in Background

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,8 @@ import * as navBarController from './controllers/navBar-controller.js';
 
 questionsController.init();
 navBarController.init();
+
+  // Mengambil elemen audio berdasarkan ID
+  var audioElement = document.getElementById("myAudio");
+  // Mengatur volume menjadi 50%
+  audioElement.volume = 0.2;
